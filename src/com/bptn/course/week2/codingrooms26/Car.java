74997fec2a08c2d5d64aa369954e681a50ff89d2
sweythak <sweythak@gmail.com>
2024-01-30 16:18:27 -0500

    Coding 2.5 second part

---
+++ src/com/bptn/course/week2/codingrooms26/Car.java
@@ -0,0 +1,21 @@
+package com.bptn.course.week2.codingrooms26;
+
+public class Car extends Vehicle {
+
+	// Step 2a: Declare String steeringWheel
+	String steeringWheel;
+
+	// Step 2b: Define a parameterized constructor as per the specifications above
+	public Car(String color, String brand, String steeringWheel ) {
+		super(color,brand); // super keyword is used to invoke parent class constructor
+		this.steeringWheel= steeringWheel;
+		
+	}
+
+	// Step 2c: Create a print() method as per the specifications above
+	@Override
+	public void print() {
+		System.out.println("I am Car");
+	}
+
+}
---
+++ src/com/bptn/course/week2/codingrooms26/Vehicle.java
@@ -0,0 +1,22 @@
+package com.bptn.course.week2.codingrooms26;
+
+public class Vehicle {
+
+	// Step 1a: Declare String color and String brand
+	String color;
+	String brand;
+
+	// Step 1b: Define a parameterized constructor with color and brand as
+	// parameters
+
+	public Vehicle(String color, String brand) {
+		this.color = color;
+		this.brand = brand;
+	}
+
+	// Step 1c: Create a print() method as described in the above.
+	public void print() {
+		System.out.println(this.color + " " + this.brand);
+	}
+
+}
